e31adc4d0ddb97d4b8551f71a1a994e5a03d5359
asaf-bg <asafbg2@gmail.com>
2020-09-09 21:57:46 +0300

    Add Sort by date
    Signed-off-by: asaf-bg <asafbg2@gmail.com>

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -20,6 +20,7 @@ export class App extends React.PureComponent<{}, AppState> {
 	};
 
 	searchDebounce: any = null;
+	sortByNewDate: boolean = true;
 
 	async componentDidMount() {
 		this.setState({
@@ -30,6 +31,11 @@ export class App extends React.PureComponent<{}, AppState> {
 	getHiddenCount(tickets: Ticket[]) {
 		return tickets.filter((t) => t.isHidden).length
 	}
+	getSearchCount(tickets: Ticket[]) {
+		return tickets.filter((t) =>
+		(t.title.toLowerCase() + t.content.toLowerCase()).includes(this.state.search.toLowerCase())).length
+	}
+
 
 	setTicketHidden(ticket: Ticket, state: boolean) {
 		ticket.isHidden = state;
@@ -51,12 +57,34 @@ export class App extends React.PureComponent<{}, AppState> {
 			return;
 		}
 
-		if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+		if (document.body.scrollTop > 200 || document.documentElement.scrollTop > 200) {
 			mybutton.style.display = "block";
 		} else {
 			mybutton.style.display = "none";
 		}
 	}
+	showbackfunction(){
+		let mybutton = document.getElementById("back_button");
+		if (mybutton === null)
+			return;
+		mybutton.style.display="inline";
+		return;
+	}
+	hidebackfunction(){
+		let mybutton = document.getElementById("back_button");
+		if (mybutton === null)
+			return;
+		mybutton.style.display="none";
+		return;
+	}
+
+	switchSort() {
+		this.sortByNewDate = !this.sortByNewDate;
+		this.forceUpdate();
+	}
+
+
+
 
 
 	renderTickets = (tickets: Ticket[]) => {
@@ -65,6 +93,15 @@ export class App extends React.PureComponent<{}, AppState> {
 		const filteredTickets = tickets
 			.filter((t) => (t.title.toLowerCase() + t.content.toLowerCase()).includes(this.state.search.toLowerCase()))
 			.filter((t) => !t.isHidden);
+
+		/* Sort tickets by date */
+		filteredTickets.sort((t1: Ticket, t2: Ticket) => {
+			if (this.sortByNewDate) {
+				return t2.creationTime - t1.creationTime;
+			}
+			return t1.creationTime - t2.creationTime;;
+		});
+
 
 		return (<ul className='tickets' >
 			{filteredTickets.map((ticket) => <li  key={ticket.id} className='ticket' >
@@ -77,7 +114,7 @@ export class App extends React.PureComponent<{}, AppState> {
 				{ticket.content}
 				</body>
 
-				<a className="problem-text-show-button" onClick={ () => this.flipTextLessMore(ticket) }> { ticket.isLess ? "Show Less" : "Show more" } </a>
+				<a className="problem-text-show-button"  onClick={ () => this.flipTextLessMore(ticket) }> { ticket.isLess ? "Show Less" : "Show more" } </a>
 
 				<footer className="boxed-container" >
 
@@ -91,7 +128,7 @@ export class App extends React.PureComponent<{}, AppState> {
 					<div
 						className='meta-data'>By {ticket.userEmail} | {new Date(ticket.creationTime).toLocaleString()}</div>
 				</footer>
-				<button className='comment_button' onClick={ () => alert("FUCK!")}>Comment</button>
+
 
 
 			</li>)}
@@ -114,22 +151,38 @@ export class App extends React.PureComponent<{}, AppState> {
 		const {tickets} = this.state;
 
 
+
 		return (<main>
 			<h1>Tickets List</h1>
 			<header >
 				<input type="search" placeholder="Search..." onChange={(e) => this.onSearch(e.target.value)}/>
 			</header>
+			<div>
+				<button id='back_button' className='hash_button' style={{display:"none" }} onClick={ () => {this.setState({search:''}) ; this.hidebackfunction()}}>⬅</button>
+				<button className='hash_button' onClick={ () => {this.setState({search:'important'}) ; this.showbackfunction()}}>Important</button>
+				<button className='hash_button' onClick={ () => {this.setState({search:'css'}) ; this.showbackfunction()}}>css</button>
+				<button className='hash_button' onClick={ () => {this.setState({search:'database'}) ; this.showbackfunction()}}>Database</button>
+				<button className='hash_button' onClick={ () => {this.setState({search:'corvid'}) ; this.showbackfunction()}}>#Corvid</button>
 
+
+			</div>
 			<div className='results-container'>
-			{tickets ? <div className='results'>Showing {tickets.length - this.getHiddenCount(tickets)} results</div> : null }
+			{tickets ? <div className='results'>Showing {this.getSearchCount(tickets) - this.getHiddenCount(tickets)} results</div> : null }
 			{ tickets && this.getHiddenCount(tickets) > 0 ?
 				<div className='results'>
 					({this.getHiddenCount(tickets)} Hidden ticket{this.getHiddenCount(tickets) > 1 ? 's': ''} -
 					<a onClick={ () => tickets.map( (t) => this.setTicketHidden(t, false) )}>restore</a>)
 				</div> : null}
+				<div className="sort-by">
+					<div className="toggle-container" onClick={() => this.switchSort()}>
+						<div className={`dialog-button ${this.sortByNewDate ? "" : "disabled"}`}>
+							{this.sortByNewDate ? "NEW " : "OLD"}
+						</div>
+					</div>
+				</div>
 			</div>
-			<button id="top-btn" className='Top_button-show'
-					onClick={()=>this.Topfunction()}>Top</button>
+			<button id="top-btn" style={{display:"none"}} className='Top_button-show'
+					onClick={()=>this.Topfunction()}>⬆</button>
 			{tickets ? this.renderTickets(tickets) : <h2>Loading..</h2>}
 
 		</main>)
